e564a8bd6cfb93d4d46e94e5f431a7791e525ea2
kristinho <kristinho@berkeley.edu>
2018-07-22 01:56:16 -0400

    Started on creating models

--- life/models.py
+++ life/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Group(models.Model):
-	established = models.DateTimeField(auto_now_add=True)
-	name = models.CharField(max_length=50)
+class EcoAction(models.Model):
+  action_name = models.CharField(max_length=200)
+  point_value = models.IntegerField()
+  
+  def __str__(self):
+    return 'Action: ' + self.action_name
+
+class EcoUser(User): # extend Django's built in User model, which already has username + password
+  eco_actions = models.ManyToManyField(EcoAction)
+  points_earned = eco_actions.count * 10 # TO DO: figure out how to calculate on the fly...
+    
