2f47dca1b256bea6e4f7df45760d29b357fc4a1a
kristinho <kristinho@berkeley.edu>
2018-07-22 16:24:32 -0400

    Fixed models based on shell testing

--- life/models.py
+++ life/models.py
@@ -2,16 +2,28 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class EcoAction(models.Model):
+  """ A model that connects an ecologically friendly action to its point value."""
+  
   action_name = models.CharField(max_length=200)
   point_value = models.IntegerField()
   
   def __str__(self):
-    return 'Action: ' + self.action_name
+    return self.action_name
 
-class EcoUser(User): # extend Django's built in User model, which already has username + password
-  eco_actions = models.ManyToManyField(EcoAction)
-
+class EcoUser(User): 
+  """ Extends Django's built in User model, which already has properties such as
+  username and password."""
+  
+  eco_actions = models.ManyToManyField(EcoAction)  
+  
+  def __str__(self):
+    return self.first_name + ' ' + self.last_name
+  
   @property
-  def points_earned():
-    return eco_actions.all().count() * 10
+  def points_earned(self):
+    total_points = 0
+    for action in self.eco_actions.all(): 
+      total_points += action.point_value
+    return total_points
+  
  
